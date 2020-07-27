4d34ae75d902b994d422f6d0024699d6f4729514
Bruno Santos <bruno_cris_santos@hotmail.com>
2020-07-27 18:21:27 -0300

    Atulização de conteudos com iframes

---
+++ _javascript/funcoes.js
@@ -0,0 +1,9 @@
+var evento = document.getElementById("menu");
+evento.addEventListener('click', MudaFoto)
+
+function MudaFoto() {
+   var back = document.getElementById ('interface')
+   back.style.backgroundColor = "red";
+
+
+}
