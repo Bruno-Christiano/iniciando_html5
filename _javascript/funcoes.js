95846cf5b6bfd1a7d80956db9c680ebc266be6bb
Bruno Santos <bruno_cris_santos@hotmail.com>
2020-09-03 08:56:42 -0300

    modificações

--- _javascript/funcoes.js
+++ _javascript/funcoes.js
@@ -1,9 +1,28 @@
-var evento = document.getElementById("menu");
-evento.addEventListener('click', MudaFoto)
+/*
+    var evento = window.document.getElementById ('home')
+    var evento2 = window.document.getElementById ('specs')
+    var eventExit = window.document.getElementById ('home')
+    
+    evento.addEventListener ('mouseover', MudaFotoHome)
+    
+    eventExit. addEventListener ('mouseout', MudaExit)
+    
+    
+    function MudaFotoHome() {
+        
+            document.getElementById ("icone").src = "_imagens/home.png"
+    }
+    
+    function MudaFotoSpecs() {
+        document.getElementById('icone').src = "_imagens/especificacoes.png"
+    }
+    
+    function MudaExit() {
+        document.getElementById ("icone").src = "_imagens/glass-oculos-preto-peq.png"
+    }
+*/
 
-function MudaFoto() {
-   var back = document.getElementById ('interface')
-   back.style.backgroundColor = "red";
-
-
+function mudaFoto(foto) {
+        
+    document.getElementById ("icone").src = foto
 }
